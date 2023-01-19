b6eeec901343cc1b6240cf87e7feb960f7f727e8
aimen <artem.igorevich8@rambler.ru>
2023-01-19 22:03:15 +0300

    fix problem with icon in detail page

--- components/event-detail/logistics-item.tsx
+++ components/event-detail/logistics-item.tsx
@@ -1,15 +1,15 @@
-import { FC } from 'react';
+import { FC, FunctionComponent } from 'react';
 import classes from './logistics-item.module.css';
 
 interface LogisticsItemProps {
-  icon: JSX.Element;
+  icon: FunctionComponent<{}>;
   children: JSX.Element[] | JSX.Element | string;
 }
 
-const LogisticsItem: FC<LogisticsItemProps> = ({ icon, children }) => {
+const LogisticsItem: FC<LogisticsItemProps> = ({ icon: Icon, children }) => {
   return (
     <li className={classes.item}>
-      <span className={classes.icon}>{icon}</span>
+      <span className={classes.icon}>{<Icon />}</span>
       <span className={classes.content}>{children}</span>
     </li>
   );
