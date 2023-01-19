6e5165765f581cd00bdc5ff42fe2903185cd1653
aimen <artem.igorevich8@rambler.ru>
2023-01-19 21:46:20 +0300

    add fetching of one event on event page

--- api/events.ts
+++ api/events.ts
@@ -15,6 +15,18 @@ export async function getAllEvents(): Promise<Event[]> {
   return events;
 }
 
+export async function getEvent(id: string): Promise<Event> {
+  const response = await fetch(
+    `https://events-app-6689b-default-rtdb.europe-west1.firebasedatabase.app/events/${id}.json`
+  );
+  const data: Omit<Event, 'id'> = await response.json();
+  const event = {
+    id,
+    ...data,
+  };
+  return event;
+}
+
 export function getFeaturedEvents(events: Event[]) {
   return events.filter((event) => event.isFeatured);
 }
--- pages/events/[eventId].tsx
+++ pages/events/[eventId].tsx
@@ -1,22 +1,18 @@
-import { useRouter } from 'next/router';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { FC } from 'react';
+import { getEvent, getAllEvents } from '../../api/events';
 import EventContent from '../../components/event-detail/event-content';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventSummary from '../../components/event-detail/event-summary';
 import ErrorAlert from '../../components/ui/error-alert/error-alert';
-import { getEventById } from '../../dummy-data';
+import { Event } from '../../types/mainTypes';
 
-const EventDetail = () => {
-  const router = useRouter();
+interface EventDetailProps {
+  selectedEvent: Event;
+}
 
-  const eventId = router.query.eventId;
-
-  let event = null;
-
-  if (typeof eventId === 'string') {
-    event = getEventById(eventId);
-  }
-
-  if (!event) {
+const EventDetail: FC<EventDetailProps> = ({ selectedEvent }) => {
+  if (!selectedEvent) {
     return (
       <ErrorAlert>
         <p>No event found!</p>
@@ -26,18 +22,50 @@ const EventDetail = () => {
 
   return (
     <>
-      <EventSummary title={event.title} />
+      <EventSummary title={selectedEvent.title} />
       <EventLogistics
-        address={event.location}
-        date={event.date}
-        image={event.image}
-        imageAlt={event.title}
+        address={selectedEvent.location}
+        date={selectedEvent.date}
+        image={selectedEvent.image}
+        imageAlt={selectedEvent.title}
       />
       <EventContent>
-        <p>{event.description}</p>
+        <p>{selectedEvent.description}</p>
       </EventContent>
     </>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  let event = {};
+
+  if (context.params) {
+    const eventId = context.params.eventId;
+    if (typeof eventId === 'string') {
+      event = await getEvent(eventId);
+    }
+  }
+
+  return {
+    props: {
+      selectedEvent: event,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const events = await getAllEvents();
+
+  const paths = events.map((event) => ({
+    params: {
+      eventId: event.id,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
 };
 
 export default EventDetail;
